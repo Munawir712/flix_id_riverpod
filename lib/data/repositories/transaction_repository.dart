618b6a4dba77d707d2fcd5bea9365e688f5f676a
munawir712 <munawirjr2@gmail.com>
2024-01-02 10:55:45 +0700

    implement firebase_transaction_repository

--- lib/data/repositories/transaction_repository.dart
+++ lib/data/repositories/transaction_repository.dart
@@ -5,5 +5,5 @@ import '../../domain/entities/result.dart';
 abstract interface class TransactionRepository {
   Future<Result<Transaction>> createTransaction(
       {required Transaction transaction});
-  Future<Result<Transaction>> getUserTransaction({required String uid});
+  Future<Result<List<Transaction>>> getUserTransactions({required String uid});
 }
